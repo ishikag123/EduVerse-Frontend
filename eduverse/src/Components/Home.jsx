e7b2d4acce9ecfc9ab3d9abaeeb5e07b39fc2218
Ishika <ishika.ighosh@gmail.com>
2024-05-24 03:08:14 +0530

    first commit

---
+++ eduverse/src/Components/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../Styles/Home.css";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <div className="text">
+        <h1 className="font-bold text-[#0B5078] text-5xl">EduVerse</h1>
+        <h3 className="text-left text-gray-700">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem tempore
+          ipsam mollitia, natus quidem autem quia modi, delectus nisi, saepe
+          quis! Aliquid laboriosam asperiores exercitationem minima eum rerum
+          error doloribus! Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Rerum, quidem amet sapiente ullam, architecto, eum non assumenda
+          facilis minima minus dolorem quam impedit iste sit alias earum
+          placeat. Quam, ex!
+        </h3>
+      </div>
+      <div className="triangle-right"></div>
+    </div>
+  );
+};
